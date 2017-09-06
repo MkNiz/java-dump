160d844b9b7f0a44c5b49e2d7809ee25e9c66a7a
Michael Nizzari <michael.nizzari@gmail.com>
2017-09-06 10:20:31 -0400

    006-variables-chars

---
+++ 006-variables-chars/VarChar.java
@@ -0,0 +1,18 @@
+public class VarChar
+{
+  public static void main(String[] args)
+  {
+    // Declaration
+    char c1, c2, c3, c4, c5;
+
+    // Assignment
+    c1 = 'H';
+    c2 = 'e';
+    c3 = 'l';
+    c4 = 'l';
+    c5 = 'o';
+
+    // Concatenation
+    System.out.println("My message: " + c1 + c2 + c3 + c4 + c5);
+  }
+}
---
+++ 006-variables-chars/readme.md
@@ -0,0 +1,4 @@
+# Variables - Characters
+
+Demonstrates storing characters in variables, and using them
+to build output text.
