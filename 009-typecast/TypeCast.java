b5ee155156d66f4baf7ddd95d22d942164e837df
Michael Nizzari <michael.nizzari@gmail.com>
2017-09-06 12:42:50 -0400

    009 - Type Cast

---
+++ 009-typecast/TypeCast.java
@@ -0,0 +1,14 @@
+public class TypeCast
+{
+  public static void main(String[] args)
+  {
+    // Create initial double
+    double myMoney = 17.25;
+
+    // Display as a double
+    System.out.println("I have $" + myMoney + ".");
+
+    // Convert to an int, and display that int
+    System.out.println("That means my dollar bills are worth " + (int)myMoney + " dollars.");
+  }
+}
---
+++ 009-typecast/readme.md
@@ -0,0 +1,4 @@
+# Type Cast
+
+Demonstrates simple type casting by converting a double
+into an int.
