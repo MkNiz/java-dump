00f030d29ffb4010d5dfc12e05d2ba2e02dbd391
Michael Nizzari <michael.nizzari@gmail.com>
2017-09-06 10:09:16 -0400

    004 - Variables - Integers

---
+++ 004-variables-integers/VarInt.java
@@ -0,0 +1,28 @@
+public class VarInt
+{
+  public static void main(String[] args)
+  {
+    // Declaration
+    int n1, n2;
+
+    // Assignment
+    n1 = 7;
+    n2 = 13;
+
+    // Addition
+    System.out.println("7 + 13 = " + (n1 + n2));
+
+    // Subtraction
+    System.out.println("7 - 13 = " + (n1 - n2));
+
+    // Multiplication
+    System.out.println("7 * 13 = " + (n1 * n2));
+
+    // Division
+    System.out.println("7 / 13 = " + (n1 / n2));
+
+    // Modulus
+    System.out.println("7 % 13 = " + (n1 % n2));
+    
+  }
+}
---
+++ 004-variables-integers/readme.md
@@ -0,0 +1,4 @@
+# Variables - Integers
+
+Demonstrates storing integers in variables, and performing basic
+integer mathematics using them.
