9d75540345db85df04cfbbd445364b813a82e077
Michael Nizzari <michael.nizzari@gmail.com>
2017-09-06 10:16:09 -0400

    005-variables-doubles

---
+++ 005-variables-doubles/VarDoub.java
@@ -0,0 +1,27 @@
+public class VarDoub
+{
+  public static void main(String[] args)
+  {
+    // Declaration
+    double d1, d2;
+
+    // Assignment
+    d1 = 4.54;
+    d2 = 121.23;
+
+    // Addition
+    System.out.println(d1 + " + " + d2 + " = " + (d1 + d2));
+
+    // Subtraction
+    System.out.println(d1 + " - " + d2 + " = " + (d1 - d2));
+
+    // Multiplication
+    System.out.println(d1 + " * " + d2 + " = " + (d1 * d2));
+
+    // Division
+    System.out.println(d1 + " / " + d2 + " = " + (d1 / d2));
+
+    // Modulus
+    System.out.println(d1 + " % " + d2 + " = " + (d1 % d2));
+  }
+}
---
+++ 005-variables-doubles/readme.md
@@ -0,0 +1,4 @@
+# Variables - Doubles
+
+Demonstrates storing doubles (floats in some other languages), and performing
+basic decimal mathematics using them.
