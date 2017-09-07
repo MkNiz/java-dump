c9bc918c8992ec236015f2b5712485969c38f3ee
Michael Nizzari <michael.nizzari@gmail.com>
2017-09-07 09:57:23 -0400

    010 - Order of Operations

---
+++ 010-order-of-operations/OpsOrder.java
@@ -0,0 +1,8 @@
+public class OpsOrder
+{
+  public static void main(String[] args)
+  {
+    System.out.println("10 * 2 / 2 + 3 - 5 = " + (10 * 2 / 2 + 3 - 5));
+    System.out.println("(10 * 2) / (2 + 3) - 5 = " + ((10 * 2) / (2 + 3) - 5));
+  }
+}
---
+++ 010-order-of-operations/readme.md
@@ -0,0 +1,4 @@
+# Order of Operations
+
+Demonstrates that Java follows the standard rules for order of operations,
+including the prioritization of parentheses.
