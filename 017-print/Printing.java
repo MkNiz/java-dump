4bf821f4a2d0ce4ff2ebe610e2e6d3323b7110d9
Michael Nizzari <michael.nizzari@gmail.com>
2017-09-07 11:06:06 -0400

    017 - Print

---
+++ 017-print/Printing.java
@@ -0,0 +1,10 @@
+public class Printing
+{
+  public static void main(String[] args)
+  {
+    System.out.print("I like ");
+    System.out.print("cheese, ");
+    System.out.println("but cheese");
+    System.out.println("doesn't like me!");
+  }
+}
---
+++ 017-print/readme.md
@@ -0,0 +1,4 @@
+# Print
+
+Demonstrates the difference between using System.out.print and System.out.println.
+The first does not utilize a new line, while the latter does.
