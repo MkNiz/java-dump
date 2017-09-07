6ff8b29f607868f6e73886e57afedc220232f59f
Michael Nizzari <michael.nizzari@gmail.com>
2017-09-07 10:04:58 -0400

    011 - Assignment Operators

---
+++ 011-assignment-operators/AssOps.java
@@ -0,0 +1,18 @@
+public class AssOps
+{
+  public static void main(String[] args)
+  {
+    int n = 1;
+    System.out.println("n = " + n);
+    n += 3;
+    System.out.println("n += 3 = " + n);
+    n -= 2;
+    System.out.println("n -= 2 = " + n);
+    n *= 5;
+    System.out.println("n *= 5 = " + n);
+    n /= 2;
+    System.out.println("n /= 2 = " + n);
+    n %= 1;
+    System.out.println("n %= 1 = " + n);
+  }
+}
---
+++ 011-assignment-operators/readme.md
@@ -0,0 +1,4 @@
+# Assignment Operators
+
+Demonstrates alternative assignment operators that allow operations
+to be enacted on the existing value of a variable.
